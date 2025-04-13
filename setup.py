6d987087afc677f120f13f632ff76c134d92a337
prakhardoneria <gamesprotec@gmail.com>
2025-04-13 21:13:24 +0530

    implemented gitAds

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="mongo_easy",
-    version="0.1.0",
+    version="0.1.1",
     author="Prakhar Doneria",
     author_email="prakhardoneria3@gmail.com",
     description="A simple and intuitive MongoDB helper library for everyone",
